ec90ce4ec02a750b15ff5baf4531730cea8d2d52
aManAndCode <105123221+aManAndCode@users.noreply.github.com>
2024-10-22 01:08:59 +0300

    aggregate_last_paid_click - изменения_1

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -5,30 +5,6 @@ WITH tab AS (
         se.medium AS utm_medium,
         se.campaign AS utm_campaign,
         COUNT(DISTINCT se.visitor_id) AS visitors_count,
-        (
-            SELECT SUM(inner_ads.daily_spent)
-            FROM (
-                SELECT
-                    ya.utm_source,
-                    ya.utm_medium,
-                    ya.utm_campaign,
-                    ya.utm_content,
-                    ya.daily_spent
-                FROM ya_ads AS ya
-                UNION ALL
-                SELECT
-                    vk.utm_source,
-                    vk.utm_medium,
-                    vk.utm_campaign,
-                    vk.utm_content,
-                    vk.daily_spent
-                FROM vk_ads AS vk
-            ) AS inner_ads
-            WHERE
-                inner_ads.utm_source = se.source
-                AND inner_ads.utm_medium = se.medium
-                AND inner_ads.utm_campaign = se.campaign
-        ) AS total_cost,
         COUNT(le.lead_id) AS lead_count,
         COUNT(le.closing_reason) AS purchases_count,
         SUM(le.amount) AS revenue,
@@ -45,14 +21,61 @@ WITH tab AS (
         se.source,
         se.medium,
         se.campaign
+),
+
+ya_vk_spent AS (
+    SELECT
+        ya.utm_source,
+        ya.utm_medium,
+        ya.utm_campaign,
+        ya.utm_content,
+        ya.daily_spent
+    FROM ya_ads AS ya
+    UNION ALL
+    SELECT
+        vk.utm_source,
+        vk.utm_medium,
+        vk.utm_campaign,
+        vk.utm_content,
+        vk.daily_spent
+    FROM vk_ads AS vk
+),
+
+ads_costs AS (
+    SELECT
+        ya_vk_spent.utm_source,
+        ya_vk_spent.utm_medium,
+        ya_vk_spent.utm_campaign,
+        SUM(ya_vk_spent.daily_spent) AS total_cost
+    FROM ya_vk_spent
+    GROUP BY
+        ya_vk_spent.utm_source,
+        ya_vk_spent.utm_medium,
+        ya_vk_spent.utm_campaign
 )
-SELECT * FROM tab
-WHERE rn = 1
+
+SELECT
+    tab.visit_date,
+    tab.utm_source,
+    tab.utm_medium,
+    tab.utm_campaign,
+    tab.visitors_count,
+    ac.total_cost,
+    tab.lead_count,
+    tab.purchases_count,
+    tab.revenue
+FROM ads_costs AS ac
+LEFT JOIN tab
+    ON ac.utm_source = tab.utm_source
+    AND ac.utm_medium = tab.utm_medium
+    AND ac.utm_campaign = tab.utm_campaign
+WHERE tab.rn = 1
 ORDER BY
-    revenue DESC NULLS LAST,
-    visit_date ASC,
-    visitors_count DESC,
-    utm_source ASC,
-    utm_medium ASC,
-    utm_campaign ASC
+    tab.revenue DESC NULLS LAST,
+    ac.total_cost DESC NULLS LAST,
+    tab.visit_date ASC,
+    tab.visitors_count DESC,
+    tab.utm_source ASC,
+    tab.utm_medium ASC,
+    tab.utm_campaign ASC
 LIMIT 15;
